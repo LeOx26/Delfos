80ca54fbf631dcfda8935646609c3e883e03b478
Leonel Benitez <leoalejandro26@gmail.com>
2023-08-22 11:28:53 -0500

    add jax

--- back/main.py
+++ back/main.py
@@ -1,1 +1,19 @@
-import dash
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class mensaje:
+    msg: str
+
+@app.get("/")
+async def root():
+    msg = 'Hola Mundo'
+    id = 10
+    return {
+            'msg':msg,
+            'id':id
+            }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
